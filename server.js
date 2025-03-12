6c8aace0ef4a0c77eb681cbf3bfeefdb3ac09ddf
Kimi <104141050+KimiVe@users.noreply.github.com>
2025-03-12 13:25:53 +0200

    fixing

--- server.js
+++ server.js
@@ -2,19 +2,16 @@ require("dotenv").config();
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
-
+const urldb = 'mysql://root:mKGXwSOJLDMHFkQfaFxkjtGSBjMvTaEJ@mysql.railway.internal:3306/railway'
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+const connection = mysql.createConnection({
+    urldb
 });
 
-db.connect((err) => {
+connection.connect((err) => {
     if (err) {
         console.error("Database connection failed:", err.stack);
         return;
